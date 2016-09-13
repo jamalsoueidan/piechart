251b02032da7ff2f887007b2b3db10aa6a6984cf
Yamal Soueidan <jamal@soueidan.com>
2016-09-13 18:57:37 +0200

    Updated Pie-chart

--- src/js/pie-chart/pie-chart.js
+++ src/js/pie-chart/pie-chart.js
@@ -5,7 +5,7 @@ class PieChart extends Component {
   constructor(props) {
     super(props);
     this.state = {data: props.data}
-    this.elementId = props.data[0].label + Math.floor((Math.random() * 1000) + 1); // just random ids for svg element
+    this.elementId = props.data[0].label + Math.floor((Math.random() * 1000) + 1); // just random id for svg element
   }
 
   componentDidMount() {
@@ -15,25 +15,22 @@ class PieChart extends Component {
     var degrees = data.reduce( (previousValue, currentValue) => previousValue + currentValue.value, 0);
 
     // calculate each sector and set all values
+    var startAngle = 0; var endAngle = 0; var procent;
     data = data.map(o => {
-      o.procent = PieChart.procent(o.value, degrees);
+      procent = PieChart.procent(o.value, degrees)
+      startAngle = endAngle;
+      endAngle = startAngle + procent;
+      o.procent = procent;
+      o.lineTo = { x: PieChart.x(startAngle), y: PieChart.y(startAngle) }
+      o.ellipticalArcs = {x: PieChart.x(endAngle), y:PieChart.y(endAngle) }
       return o;
     })
 
-    // sort sectors by procent
-    data = data.sort( (a, b) => b.procent-a.procent );
-
     // draw pie chart
     var draw = SVG(this.elementId).viewbox({x: 0, y: 0, width: 400, height: 400});
-    var startAngle = 0; var endAngle = 0; var arc_flag = 0;
-    console.log(startAngle);
     data.forEach(o => {
-      arc_flag = 0;
-      startAngle = endAngle;
-      endAngle = startAngle + o.procent;
-      if (o.procent>180) { arc_flag = 1; }
-      var arc = draw.path("M200,200  L" + PieChart.x(startAngle) + "," + PieChart.y(startAngle)+ "  A180,180 0 " + arc_flag + ",1 " + PieChart.x(endAngle) + "," + PieChart.y(endAngle) + " z");
-      arc.fill(o.color);
+      draw.path("M200,200  L" + o.lineTo.x + "," + o.lineTo.y + "  A180,180 0 " + (o.procent > 180 ? "1" : "0") + ",1 " + o.ellipticalArcs.x + "," + o.ellipticalArcs.y + " z")
+          .fill(o.color);
     });
   }
 
